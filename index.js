7359f3f4ef39b4ec1d4a651ffadc1ffd5f754b00
palevoo <kud.nikita@gmail.com>
2018-05-17 17:06:28 +0200

    refactored variables

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 
-const dayInMil = 86400000;
-const now = Date.now(); // milliseconds
-const date = new Date(); // eg Sun May 13 2018 17:23:02 GMT+0300 (MSK)
-const year = date.getFullYear(); // eg 2018
-const dayOne = new Date(year, 0, 1); // human
-const dayZero = dayOne.getTime(); // eg Mon Jan 01 2018 00:00:00 GMT+0300 (MSK)
-const daysPast = Math.floor((now - dayZero) / dayInMil); // Days eg. 132
-const dayPassed = '[x]';
-const dayLeft = '[ ]';
+const DAY_IN_MILLISECONDS = 86400000;
+const NOW = new Date(); // eg Sun May 13 2018 17:23:02 GMT+0300 (MSK)
+const NOW_IN_MILLISECONDS = Date.now(); // milliseconds
+const YEAR = NOW.getFullYear(); // eg 2018
+const DAY_ONE = new Date(YEAR, 0, 1); // human
+const DAY_ONE_IN_MILLISECONDS = DAY_ONE.getTime(); // eg Mon Jan 01 2018 00:00:00 GMT+0300 (MSK)
+
+const daysPast = Math.floor((NOW_IN_MILLISECONDS - DAY_ONE_IN_MILLISECONDS) / DAY_IN_MILLISECONDS); // Days eg. 132
 const progressBar = Math.floor(daysPast/ 3.65)
 
 const array = new Array(365);
@@ -26,4 +25,4 @@ const array3 = array.map((cur, ind) => {
 
 exports.runner = function() {
   console.log(`                           Nintai\n${array3.join('')} Progress: ${daysPast} / 365 Days | ${progressBar}%`);
-}
+};
