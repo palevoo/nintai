4e37bd5f8e2172ab2c4c8efd878e914d47b3a354
palevoo <kud.nikita@gmail.com>
2018-05-19 17:14:11 +0200

    inquirer now set the mvp config file

--- inquirer.js
+++ inquirer.js
@@ -1,38 +1,38 @@
-const inquirer = require('inquirer');
-const dateValidator = require('./utils/dateValidator');
-
-console.log(dateValidator);
+const inquirer = require("inquirer");
+const dateValidator = require("./utils/dateValidator");
+const config = require("./config.js");
 
 const questions = [
   {
-  type: 'input',
-  name: 'date',
-  message: 'Please choose the countdown beginning',
-  validate: dateValidator.validator,
-  default: '2018-01-01'
+    type: "input",
+    name: "date",
+    message: "Please choose the countdown beginning",
+    validate: dateValidator.validator,
+    default: "2018-01-01"
   },
   {
-  type: 'list',
-  name: 'view',
-  message: 'Please choose visualisation option?',
-  choices: ['Day', 'Percentage Bar'],
-  default: 'Day',
-  },
-  {
-  type: 'confirm',
-  name: 'launch',
-  message: 'Run during terminal launch?',
-  default: 'Nope, all good!'
-  },
+    type: "list",
+    name: "view",
+    message: "Please choose visualisation option?",
+    choices: ["day", "percent"],
+    default: "day"
+  }
+  // {
+  // type: 'confirm',
+  // name: 'launch',
+  // message: 'Run during terminal launch?',
+  // default: 'Nope, all good!'
+  // },
   // {
   // type: 'confirm',
   // name: 'confirm',
   // message: 'Is everything correct?',
   // },
-]
+];
+
 exports.questionMe = () => {
-  inquirer.prompt(questions)
-    .then(answers => {
-      console.log(answers);
+  inquirer.prompt(questions).then(answers => {
+    console.log(answers);
+    config.setter(answers);
   });
-}
+};
