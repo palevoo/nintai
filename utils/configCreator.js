ed170e0896513a2175e32582f49470bf799c984f
palevoo <kud.nikita@gmail.com>
2018-05-19 17:16:54 +0200

    removed legacy code

--- utils/configCreator.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-exports.writeFile = () => {
-  fs.writeFileSync('./config.js', input, (err) => {
-      if (err) throw err;
-      console.log('Config file successfully created!');
-  });
-}
